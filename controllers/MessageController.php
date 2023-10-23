8247db463fe94f99188c566d66c5984bc802aeb1
Renardo Tunggara <renardotunggara@gmail.com>
2023-10-23 19:06:44 +0700

    files

---
+++ controllers/MessageController.php
@@ -0,0 +1,32 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] === "POST")
+{
+    if(isset($_POST['send']))
+    {
+        $title = $_POST['title'];
+        $recipient = $_POST['recipient'];
+        $message = $_POST['message'];
+        $user_file = $_FILES['user_file'];
+
+        $target_directory = "../uploads/";
+        $file_name = $user_file ['name'];
+        $random_id = uniqid();
+        $file_name = "$random_id - $file_name";
+
+
+        if($user_file['size']>25000000){
+            echo"File is too big!";
+        }
+        else{
+            if(move_uploaded_file($user_file["tmp_name"],$target_directory . $file_name)){
+                echo"success";
+            }
+            else{
+                echo"asdsadadewasdasda";
+            }
+        }
+        
+    }
+}
+
+?>  
